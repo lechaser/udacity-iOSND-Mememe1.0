2859a342d2cf8c516ded33360da94e8a8b20be35
Sergio Lecha <sergio.lecha@adidas.com>
2019-04-11 08:39:41 +0200

    Initial Commit

---
+++ mememe_iosnd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mememe_iosnd
+//
+//  Created by Lecha, Sergio (formerly Lecha Lecuona) on 11/04/2019.
+//  Copyright © 2019 Sergio SL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mememe_iosndTests/mememe_iosndTests.swift
@@ -0,0 +1,34 @@
+//
+//  mememe_iosndTests.swift
+//  mememe_iosndTests
+//
+//  Created by Lecha, Sergio (formerly Lecha Lecuona) on 11/04/2019.
+//  Copyright © 2019 Sergio SL. All rights reserved.
+//
+
+import XCTest
+@testable import mememe_iosnd
+
+class mememe_iosndTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
